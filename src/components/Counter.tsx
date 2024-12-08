fc8da7f6faec5c401b550b266bf9f6f962e93e7b
RodrigoSHL <shl.catalan@gmail.com>
2024-12-07 22:29:40 -0300

    feat: install react and create component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export const Counter = () => {
+
+    const [counter, setCounter] = useState(0)
+    return (
+        <>
+            <h1>Counter</h1>
+            <h3>value: {counter}</h3>
+
+            <button onClick={() => setCounter(prev => ++prev)} type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">+1</button>
+            <button onClick={() => setCounter(prev => --prev)} type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">-1</button>
+
+        </>
+    )
+}
